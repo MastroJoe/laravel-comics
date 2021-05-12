0c4ba9ac246c33d685ca23453ce14871e277625b
Giorgio <giorgio.pignattari@gmail.it>
2021-05-12 16:12:40 +0200

    creazione progetto con Laravel

---
+++ laravel-comics/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('main')
+
+<div class="container">
+
+</div>
+
+@endsection
---
+++ laravel-comics/resources/views/layouts/app.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+    </head>
+    <body>
+      <header>
+
+
+        <div class="links">
+            <a href="https://laravel.com/docs">Docs</a>
+            <a href="https://laracasts.com">Laracasts</a>
+            <a href="https://laravel-news.com">News</a>
+            <a href="https://blog.laravel.com">Blog</a>
+            <a href="https://nova.laravel.com">Nova</a>
+            <a href="https://forge.laravel.com">Forge</a>
+            <a href="https://vapor.laravel.com">Vapor</a>
+            <a href="https://github.com/laravel/laravel">GitHub</a>
+        </div>
+
+
+      </header>
+      <main>
+        @yield('main')
+      </main>
+      <footer>
+
+      </footer>
+    </body>
+</html>
