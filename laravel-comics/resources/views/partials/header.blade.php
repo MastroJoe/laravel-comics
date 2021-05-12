0bf66acc1dcd0f3acbb352f4b98b0e0c7a044ff2
Giorgio <giorgio.pignattari@gmail.it>
2021-05-12 17:48:51 +0200

    struttura header

--- laravel-comics/resources/views/partials/header.blade.php
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -1,14 +1,38 @@
-<header>
-
-  <div class="links">
-      <a href="https://laravel.com/docs">Docs</a>
-      <a href="https://laracasts.com">Laracasts</a>
-      <a href="https://laravel-news.com">News</a>
-      <a href="https://blog.laravel.com">Blog</a>
-      <a href="https://nova.laravel.com">Nova</a>
-      <a href="https://forge.laravel.com">Forge</a>
-      <a href="https://vapor.laravel.com">Vapor</a>
-      <a href="https://github.com/laravel/laravel">GitHub</a>
+<header class="container-fluid">
+  <div class="header-top container-fluid">
+    <!-- due link sulla destra -->
+    <div class="container">
+      <ul>
+        <li><a href="#">dc power visa</a></li>
+        <li><a href="#">additional dc sites</a></li>
+      </ul>
+    </div>
+    <!-- menu navbar con logo e ricerca-->
+    <div class="header-bottom container-fluid">
+      <div class="container">
+        <!-- logo -->
+        <a class="upper" href="">
+          <img src="" alt="">
+        </a>
+        <!-- navbar menu -->
+        <ul class="navbar">
+          <li><a class="upper" href=""></a>characters</li>
+          <li><a class="upper" href=""></a>comics</li>
+          <li><a class="upper" href=""></a>movies</li>
+          <li><a class="upper" href=""></a>tv</li>
+          <li><a class="upper" href=""></a>games</li>
+          <li><a class="upper" href=""></a>collectibles</li>
+          <li><a class="upper" href=""></a>videos</li>
+          <li><a class="upper" href=""></a>fans</li>
+          <li><a class="upper" href=""></a>news</li>
+          <li><a class="upper" href=""></a>shop</li>
+        </ul>
+        <!-- ricerca -->
+        <div class="search">
+          <input type="text" name="Search" value="" placeholder="Search">
+        </div>
+        </div>
+      </div>
+    </div>
   </div>
-
 </header>
--- laravel-comics/routes/web.php
+++ laravel-comics/routes/web.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $database = config('comics');
+    return view('home')->with('comics', $database);
 })->name('home');
-
-// Route::get('/', function () {
-//     return view('home');
-// })->name('home');
